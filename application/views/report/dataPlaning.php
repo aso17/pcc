de4d2fc88beef060c0678d3d280cc54d0174b882
aso17 <60882149+aso17@users.noreply.github.com>
2021-07-22 00:44:05 +0700

    size

--- application/views/report/dataPlaning.php
+++ application/views/report/dataPlaning.php
@@ -1,53 +1,6 @@
 <!-- Main content -->
 <style>
-h7 {
-    padding: 0;
-    text-align: center;
 
-}
-
-h6 {
-
-    font-family: sans-serif;
-    text-align: center;
-    margin-bottom: 2%;
-}
-
-p {
-    font-family: sans-serif;
-    text-align: center;
-    margin: 0;
-}
-
-.absolute1 {
-    position: absolute;
-    width: 300px;
-    top: 0;
-    bottom: 0;
-    background-color: #ff0;
-}
-
-.absolute2 {
-    position: absolute;
-    top: 0;
-    bottom: 0;
-    left: 300px;
-    right: 200px;
-    background-color: #3c3c3c;
-}
-
-.table {
-    justify-content: end;
-}
-
-.row {
-    justify-content: end;
-
-}
-
-.card {
-    justify-items: left;
-}
 </style>
 
 <div class="row">
@@ -59,35 +12,36 @@ p {
             <div class=" card-header">
                 <h7 class="m-0 font-weight-bold text-dark text-center mt-1">PT. ADIS DIMENSION
                     FOOWEAR</h7>
-                <h6 class="m-0  text-dark text-center ">DATA PLANING</h6>
-                <p class="m-0  text-dark text-center ">Tanggal</p>
-                <p class="m-0  text-dark text-center "><?= $tgl_awal ?></p>
-                <p class="m-0  text-dark text-center ">s/d</p>
-                <p class="m-0  text-dark text-center "><?= $tgl_ahir ?></p>
-                <p class="m-0  text-dark text-right ">Tanggal cetak : <?= date('d/m/y') ?></p>
+                <h5 class="m-0  text-dark text-center " style="text-align: center; margin-top:0">DATA PLANING</h5>
+                <p class="m-0  text-dark text-center " style="text-align: center; margin-top:0px">Tanggal</p>
+                <p class="m-0  text-dark text-center " style="text-align: center; margin-top:0px"><?= $tgl_awal ?></p>
+                <p class="m-0  text-dark text-center " style="text-align: center; margin-top:0">s/d</p>
+                <p class="m-0  text-dark text-center " style="text-align: center; margin-top:0"><?= $tgl_ahir ?></p>
+                <p class="m-0  text-dark text-right " style="text-align:right; margin-top:0">Tanggal cetak :
+                    <?= date('d/m/y') ?></p>
+
+            </div>
 
 
+
+            <div class="card-body">
                 <a href="<?= ('cetak/') . $tgl_awal . "/" . $tgl_ahir ?>" class="btn btn-dark btn-icon-split btn-sm"
                     target="_BLANK">
                     <span class="icon text-white-10"><i class="fa fa-file-pdf text-light"></i>
                     </span>
                     <span class="font-weight-bold text">PDF</span></a>
-
-
-            </div>
-            <div class="card-body">
                 <div class="table table-responsive">
-                    <table class="table table-sm" id="tabel_" cellspacing="0" border="1">
+                    <table class="table table-sm" id="tabel_" cellspacing="0" style="text-align: center;">
                         <thead>
                             <tr>
-                                <th class="text-center">No</th>
-                                <th class="text-center">BOM ID</th>
-                                <th class="text-center">Kategori</th>
-                                <th class="text-center">Type Sample</th>
-                                <th class="text-center">Model Name</th>
-                                <th class="text-center">Size</th>
-                                <th class="text-center">Tanggal Spk</th>
-                                <th class="text-center">Status</th>
+                                <th class="text-center" style="width: 10px; height: 50px;">No</th>
+                                <th class="text-center" style="width: 150px;">BOM ID</th>
+                                <th class="text-center" style="width: 150px;">Kategori</th>
+                                <th class="text-center" style="width: 150px;">Type Sample</th>
+                                <th class="text-center" style="width: 150px;">Model Name</th>
+                                <th class="text-center" style="width: 150px;">Size</th>
+                                <th class="text-center" style="width: 150px;">Tanggal Spk</th>
+                                <th class="text-center" style="width: 150px;">Status</th>
 
                             </tr>
                         </thead>
@@ -95,14 +49,14 @@ p {
                             <?php $i = 1; ?>
                             <?php foreach ($spk as $sp) : ?>
                             <tr>
-                                <td class="text-center"><?= $i++ ?>.</td>
-                                <td class="text-center"><?= $sp->bom_id ?></td>
-                                <td class="text-center"><?= $sp->kategori ?></td>
-                                <td class="text-center"><?= $sp->type_sample ?></td>
-                                <td class="text-center"><?= $sp->model_name ?></td>
-                                <td class="text-center"><?= $sp->size ?></td>
-                                <td class="text-center"><?= $sp->tgl_spk ?></td>
-                                <td class="text-center"><?= $sp->status ?></td>
+                                <td class="text-center" style="width: 10px; height:auto"><?= $i++ ?>.</td>
+                                <td class="text-center" style="width: 150px;"><?= $sp->bom_id ?></td>
+                                <td class="text-center" style="width: 150px;"><?= $sp->kategori ?></td>
+                                <td class="text-center" style="width: 150px;"><?= $sp->type_sample ?></td>
+                                <td class="text-center" style="width: 150px;"><?= $sp->model_name ?></td>
+                                <td class="text-center" style="width: 150px;"><?= $sp->size ?></td>
+                                <td class="text-center" style="width: 150px;"><?= $sp->tgl_spk ?></td>
+                                <td class="text-center" style="width: 150px;"><?= $sp->status ?></td>
 
                             </tr>
                             <?php endforeach; ?>
